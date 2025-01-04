d3e53993cc3beb56c98b3ccd0bfcb62524f31bc9
nekogakure <osjin83@gmail.com>
2025-01-04 18:28:43 +0900

    説明の改善

--- catLog.h
+++ catLog.h
@@ -1,10 +1,12 @@
 /*
  *  
  *  How to use    : The CatLog() function requires a log message to be passed as an argument.
- *                  Log messages passed here will be logged to the file specified by LOG_FILE.
+ *                  Log messages passed here will be logged to the file specified by "LOG_FILE".
  *
  *                  The catLog_clear() function deletes all logs output by catLog().
- *                  The file referenced at this time is the file declared with LOG_FILE.     
+ *                  The file referenced at this time is the file declared with "LOG_FILE".    
+ *                  
+ *                  You can remove the logging feature by setting the "CATLOG" constant to false. Returns when set to true.
  *
  *                  Currently, this is the only function available, but I plan to add more as time goes on.                    
  * 
