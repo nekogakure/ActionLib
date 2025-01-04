400f2388ee5168d9da1fe83f59eaaf719eddd3ad
nekogakure <osjin83@gmail.com>
2025-01-04 18:18:02 +0900

    テストの仕方更新

---
+++ test/catLog/catLog_test.c
@@ -0,0 +1,28 @@
+#include "../../catLog.h"
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    // コマンドライン引数が '--clear' であればログをクリア
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--clear") == 0) {
+            catLog_clear();
+            catLog("Log has been cleared.");
+            return 0;  // ログがクリアされたら処理を終了
+        }
+    }
+
+    // 通常のログ処理
+    catLog("This is catLog test.");
+    
+    int i = 0;
+    
+    while (i < 10) {
+        catLog("Current value of i: ");
+        char buffer[50];
+        snprintf(buffer, sizeof(buffer), "%d", i);
+        catLog(buffer);
+        i++;
+    }
+    
+    return 0;
+}
